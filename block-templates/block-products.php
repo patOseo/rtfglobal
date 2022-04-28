53dc9a5faa7994b36c6022f26a5869a8b93c61c4
Pat Monette <patrick@ontarioseo.ca>
2022-04-28 16:03:04 +0000

    products filter

--- block-templates/block-products.php
+++ block-templates/block-products.php
@@ -1,6 +1,7 @@
 <?php
 
 $heading = get_field('heading');
+$show_all = get_field('show_all');
 $customer = get_field('customer_category');
 $product_type = get_field('type_category');
 
@@ -24,7 +25,24 @@ $args = array(
     ),
 );
 
-$products = new WP_Query($args);
+$args_all = array(
+	'post_type' => 'products',
+	'post_status' => 'publish',
+	'order' => 'ASC',
+	'orderby' => 'title'
+);
+
+if($show_all == 1) {
+	$products = new WP_Query($args_all);
+	$customer_categories = get_terms(array(
+		'taxonomy' => 'customers'
+	));
+	$type_categories = get_terms(array(
+		'taxonomy' => 'product_type'
+	));
+} else {
+	$products = new WP_Query($args);
+}
 
 ?>
 
@@ -32,17 +50,46 @@ $products = new WP_Query($args);
 <div class="products-list">
 	<div class="row">
 		<div class="col-12">
-			<?php 
-				$customer_label = get_term($customer, 'customers');
-				$product_type_label = get_term($product_type, 'product_type');
-			?>
 			<?php if($heading): ?><h2 class="text-center"><?= $heading; ?></h2><?php endif; ?>
 		</div>
+		<?php if($show_all == 1): ?>
+		<div class="col-12 mb-5">
+			<div id="filterContainer" class="text-center">
+			<button class="btn btn-secondary btn-sm mb-1 all active" onclick="filterSelection('all')">All</button>
+			<?php foreach($customer_categories as $customer_category): ?>
+				<button class="btn btn-secondary btn-sm mb-1" onclick="filterSelection('<?php echo $customer_category->slug; ?>')"><?php echo $customer_category->name; ?></button>
+			<?php endforeach; ?>
+			<?php foreach($type_categories as $type_category): ?>
+				<button class="btn btn-secondary btn-sm mb-1" onclick="filterSelection('<?php echo $type_category->slug; ?>')"><?php echo $type_category->name; ?></button>
+			<?php endforeach; ?>
+			</div>
+		</div>
+
+		<?php endif; ?>
 	</div>
 	<div class="row row-eq-height">
 		<?php while($products->have_posts()): $products->the_post(); ?>
+			<?php
+				$customer_cats = get_the_terms(get_the_ID(), 'customers');
+				$type_cats = get_the_terms(get_the_ID(), 'product_type');
+				
+				if($customer_cats) {
+					$the_customer_cats = null;
+					foreach($customer_cats as $customer_cat) {
+						$the_customer_cats .= $customer_cat->slug . " ";
+					}
+				}
+
+				if($type_cats) {
+					$the_type_cats = null;
+					foreach($type_cats as $type_cat) {
+						$the_type_cats .= $type_cat->slug . " ";
+					}
+				}
+
+			?>
 			<?php $mainimg = get_field('main_image', get_the_ID()); ?>
-			<div class="col-md-4">
+			<div class="col-md-4 filter-product-card all <?php if($the_customer_cats) { echo $the_customer_cats; } echo " "; if($the_type_cats) { echo $the_type_cats; } ?>">
 				<div class="shadowbox hover text-center">
 					<?php echo wp_get_attachment_image($mainimg, 'blog-thumb'); ?>
 					<a href="<?php the_permalink(); ?>" class="stretched-link"><h2><?php the_title(); ?></h2></a>
