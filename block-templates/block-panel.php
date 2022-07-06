31d6e371a188c4d6dd429ed1a13916e3b6997b8d
Pat Monette <patrick@ontarioseo.ca>
2022-07-06 13:24:30 +0000

    position relative on card panel block

--- block-templates/block-panel.php
+++ block-templates/block-panel.php
@@ -5,7 +5,7 @@ $htmltagclose = "</" . $htmltag . ">";
 $panel_link = get_field('link');
 ?>
 <div class="panel-block-container">
-	<div class="text-center panel-block <?php if($panel_link): ?>hover<?php endif; ?>" style="background: linear-gradient(rgb(255 255 255 / 80%),rgb(255 255 255 / 80%)), url('<?php the_field('background_image'); ?>') no-repeat center center; background-size:cover;">
+	<div class="position-relative text-center panel-block <?php if($panel_link): ?>hover<?php endif; ?>" style="background: linear-gradient(rgb(255 255 255 / 80%),rgb(255 255 255 / 80%)), url('<?php the_field('background_image'); ?>') no-repeat center center; background-size:cover;">
 		<?php echo $htmltagopen; ?><?php if($panel_link): ?><a href="<?= $panel_link; ?>" class="stretched-link"><?php endif; the_field('title'); if($panel_link): ?></a><?php endif; ?><?php echo $htmltagclose; ?>
 	</div>
 </div>
